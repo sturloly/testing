e6cf9a3d98ea9e88ee644eff7989a041b8f4e10d
sturloly <sturloly@gmail.com>
2017-07-03 00:02:08 +0800

    test funcs modulized

--- mocha-demo/test/appTest.js
+++ mocha-demo/test/appTest.js
@@ -1,27 +1,26 @@
 // test file for app.js
 
 const assert =  require('chai').assert;   // node.js default assertion library
-const sayHello = require('../app').sayHello;
-const addNumbers = require('../app').addNumbers;
+const app = require('../app');
 
 describe("App", function(){
   it('app should return hello', function() {
-    let result = sayHello();
+    let result = app.sayHello();
     assert.equal(result, 'hello');
   });
 
   it('sayHello should return type string', function(){
-    let result = sayHello();
+    let result = app.sayHello();
     assert.typeOf(result, 'string');
   });
 
   it('addNumbers should be more than 7', function(){
-    let result = addNumbers(5,5); //ramdom numbers
+    let result = app.addNumbers(5,5); //ramdom numbers
     assert.isAbove(result, 7);
   });
 
   it('addNumbers should should return type number', function(){
-    let result = addNumbers(5,5); //ramdom numbers
+    let result = app.addNumbers(5,5); //ramdom numbers
     assert.typeOf(result, 'number');
   });
 });
