d0f2a21c45ca3e5e213fee76bdc34dc19c047b73
sturloly <sturloly@gmail.com>
2017-07-03 00:05:05 +0800

    test result glob scope

--- mocha-demo/test/appTest.js
+++ mocha-demo/test/appTest.js
@@ -3,24 +3,28 @@
 const assert =  require('chai').assert;   // node.js default assertion library
 const app = require('../app');
 
+// results
+sayHelloResult = app.sayHello();
+addNumbersResult = app.addNumbers(5,5);
+
 describe("App", function(){
   it('app should return hello', function() {
-    let result = app.sayHello();
-    assert.equal(result, 'hello');
+    // let result = app.sayHello();
+    assert.equal(sayHelloResult, 'hello');
   });
 
   it('sayHello should return type string', function(){
-    let result = app.sayHello();
-    assert.typeOf(result, 'string');
+    // let result = app.sayHello();
+    assert.typeOf(sayHelloResult, 'string');
   });
 
   it('addNumbers should be more than 7', function(){
-    let result = app.addNumbers(5,5); //ramdom numbers
-    assert.isAbove(result, 7);
+    // let result = app.addNumbers(5,5); //ramdom numbers
+    assert.isAbove(addNumbersResult, 7);
   });
 
   it('addNumbers should should return type number', function(){
-    let result = app.addNumbers(5,5); //ramdom numbers
-    assert.typeOf(result, 'number');
+    // let result = app.addNumbers(5,5); //ramdom numbers
+    assert.typeOf(addNumbersResult, 'number');
   });
 });
